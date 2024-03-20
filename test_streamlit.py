61ef9d11198f4fb64e9c6f48169c05e4d6afa2be
vincenzojrs <vjrstriano@gmail.com>
2024-03-20 16:41:35 +0100

    Started the cloud version development

--- test_streamlit.py
+++ test_streamlit.py
@@ -1,10 +1,11 @@
 from streamlit_gsheets import GSheetsConnection
+import streamlit as st
 
 def import_nutritional_database_streamlit():
     # Create a connection object.
     conn = st.connection("gsheets", type=GSheetsConnection)
-    df = conn.read(worksheet="Foglio7")
+    df = conn.read(worksheet="Foglio7",
+                   ttl="60m",
+                   usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12],
+                   nrows=14,)
     return df
-    
-
-
