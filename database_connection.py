1808be7cc7536adf7d23abcf294537d4639c543d
vincenzojrs <vjrstriano@gmail.com>
2024-03-25 20:10:02 +0100

    removed 'cloud' argument

--- database_connection.py
+++ database_connection.py
@@ -4,14 +4,13 @@ from google.oauth2 import service_account
 import streamlit as st
 
 def import_nutritional_database():
-    if cloud == False:
-        conn = st.connection("gsheets", type=GSheetsConnection)
-        df = conn.read(worksheet="nutritional_database",
-                       ttl="60m",
-                       usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12],
-                       nrows=100)
-        df.dropna(how='all', inplace = True)
-        return df
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    df = conn.read(worksheet="nutritional_database",
+                   ttl="60m",
+                   usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12],
+                   nrows=100)
+    df.dropna(how='all', inplace = True)
+    return df
 
 def import_weight_database():
     conn = st.connection("gsheets", type=GSheetsConnection)
