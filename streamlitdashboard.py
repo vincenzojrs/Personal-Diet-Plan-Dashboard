040a6c5ea785cf2bafe21b2bce1de59e92a746c1
vincenzojrs <vjrstriano@gmail.com>
2024-03-26 22:23:48 +0100

    test commit su vscode

--- streamlitdashboard.py
+++ streamlitdashboard.py
@@ -35,7 +35,7 @@ with tab2:
     with col2:
         st.subheader("Macronutrients contribution")
         st.dataframe(tuesday_dataframe[-1].T, use_container_width = True)
-    st.subheader("Meal nutrients")
+    st.subheader("")
     st.dataframe(tuesday_dataframe[2], use_container_width = True)
               
 with tab3:
