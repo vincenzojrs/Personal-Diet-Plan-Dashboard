5d69d37fe83dc23e0ae9ae33d8abae5cd0f35da4
vincenzojrs <vjrstriano@gmail.com>
2024-03-26 09:30:39 +0100

    added pig favicon

--- streamlitdashboard.py
+++ streamlitdashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(layout = 'wide', page_title = 'Enzo devi dimagrire')
+st.set_page_config(layout = 'wide', page_title = 'Enzo devi dimagrire', page_icon = ':pig:')
 st.title('Enzo devi dimagrire')
 
 from main import Monday, Tuesday, Wednesday, Thursday
