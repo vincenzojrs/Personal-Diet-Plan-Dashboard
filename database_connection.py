806fa1944c8193f1bc8901ccf32ff13f5f897ea7
vincenzojrs <vjrstriano@gmail.com>
2024-03-27 13:47:21 +0100

    added write function to database connection

--- database_connection.py
+++ database_connection.py
@@ -1,25 +1,33 @@
+from oauth2client.service_account import ServiceAccountCredentials
 from streamlit_gsheets import GSheetsConnection
-import gspread
 from google.oauth2 import service_account
 import streamlit as st
+import gspread
 
-def import_nutritional_database():
+def connect_nutritional_database():
     conn = st.connection("gsheets", type=GSheetsConnection)
     df = conn.read(worksheet="nutritional_database",
-                   ttl="60m",
-                   usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12],
-                   nrows=100)
+                    ttl="60m",
+                    usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12],
+                    nrows=100)
     df.dropna(how='all', inplace = True)
     return df
 
-def import_weight_database():
-    conn = st.connection("gsheets", type=GSheetsConnection)
-    df = conn.read(worksheet="weight",
-                   ttl="60m",
-                   usecols=[0,1,2,3],
-                   nrows=1000)
-    # Drop rows where all values are missing
-    df.dropna(how='all', inplace = True)
-    # Inferring missing data using linear interpolation
-    df.interpolate(method='linear', inplace=True)
-    return df
+def connect_weight_database(mode: str = 'read', data = None):
+    if mode == 'read':
+        conn = st.connection("gsheets", type=GSheetsConnection)
+        df = conn.read(worksheet="weight",
+                    ttl="60m",
+                    usecols=[0,1,2,3],
+                    nrows=1000)
+        # Drop rows where all values are missing
+        df.dropna(how='all', inplace = True)
+        # Inferring missing data using linear interpolation
+        df.interpolate(method='linear', inplace=True)
+        return df
+    elif mode == 'write':
+        scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets['gsheets'], scope)
+        client = gspread.authorize(creds)
+        sh = client.open('PersonalPlan').worksheet('weight_to_update')
+        sh.append_row(data)
